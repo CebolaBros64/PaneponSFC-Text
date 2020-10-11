edfed17836bddfc667b45d6cce474a7f40247d8e
CebolaBros64 <vimniciusmaster@gmail.com>
2020-10-11 11:34:46 -0300

    change variable names for character table file

--- PdPSFC.py
+++ PdPSFC.py
@@ -3,14 +3,14 @@ import tbl2dict
 
 binPath = 'test.bin'
 
+charTBLPath = 'Tetris Attack.tbl'
+
 # Load binary containing text script
 with open(binPath, 'rb') as f:
     binScript = f.read()
 
-filepath = 'Tetris Attack.tbl'
+# Load character table file
+with open(charTBLPath, 'r', encoding='unicode_escape') as f:
+    charTBLFile = f.read()
 
-# Driver Code
-with open(filepath, 'r', encoding='unicode_escape') as f:
-    tblFile = f.read()
-
-print(charmap_decode(binScript, 'strict', tbl2dict.convert(tblFile)))
+print(charmap_decode(binScript, 'strict', tbl2dict.convert(charTBLFile)))
