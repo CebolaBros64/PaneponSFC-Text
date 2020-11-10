9eb59548b3694ac1b1270cb37f03f0c4950ed36c
CebolaBros64 <vimniciusmaster@gmail.com>
2020-11-10 17:41:52 -0300

    py: preliminary support for encoding offsetting

--- PdPSFC.py
+++ PdPSFC.py
@@ -6,6 +6,17 @@ binPath = 'test.bin'
 charTBLPath = 'Tetris Attack.tbl'
 codeTBLPath = 'Tetris Attack [Control Codes].tbl'
 
+def offset_encoding(_dict, n):
+    offsetDict = _dict.copy()
+    offset = n * 16
+    for entry in _dict:
+        #print(entry)
+        offsetDict[entry-offset] = _dict[entry]
+    
+    print(_dict)
+    print(offsetDict)
+    #return offsetDict
+
 # Load binary containing text script
 with open(binPath, 'rb') as f:
     binScript = f.read()
@@ -22,16 +33,27 @@ codeTBLDict = tbl2dict.convert(codeTBLFile)
 
 # Merge both dictionaries together
 TBLDict = charTBLDict.copy()
-
-#print(TBLDict == charTBLDict) --> True
-#TBLDict['peepee'] = 'poopoo'
-#print(TBLDict == charTBLDict) --> False
-
 TBLDict.update(codeTBLDict)
 
 # Profit
 #print(charmap_decode(binScript, 'strict', charTBLDict))
-#print(charmap_decode(binScript, 'strict', codeTBLDict))
-print(charmap_decode(binScript, 'strict', TBLDict))
-with open("[butt].txt", "w") as f:
-    f.write(charmap_decode(binScript, 'strict', TBLDict)[0])
+#print(charmap_decode(binScript, 'strict', codeTBLDict)) 
+#print(charmap_decode(binScript, 'strict', TBLDict))
+#with open("[butt].txt", "w") as f:
+#    f.write(charmap_decode(binScript, 'strict', TBLDict)[0])
+
+#i = 0
+#_str = ''
+#try:
+#    while True:
+#    if binScript[i]
+#    
+#        print(_str)
+#        _str += TBLDict[binScript[i]]
+#        i += 1
+#except IndexError:
+#    print("Reached end of file")
+#    print(_str)
+#   
+
+offset_encoding(TBLDict, 1)
