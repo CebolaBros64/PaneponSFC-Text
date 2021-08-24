49936294ffbc928558939fd23968f3b8f5cf7244
CebolaBros64 <15020325+CebolaBros64@users.noreply.github.com>
2021-08-24 16:59:52 -0300

    py: Cleanup

--- PdPSFC.py
+++ PdPSFC.py
@@ -1,64 +1,63 @@
-from codecs import charmap_decode
 import tbl2dict
 
 binPath = 'test.bin'
-
 charTBLPath = 'Tetris Attack.tbl'
 codeTBLPath = 'Tetris Attack [Control Codes].tbl'
 
+
 def offset_encoding(_dict, n):
-    '''Offsets the keys of a dictionary by (entry-(n*16))'''
-    offsetDict = _dict.copy()
+    """Offsets the keys of a dictionary by (entry-(n*16))"""
+    offset_dict = _dict.copy()
     offset = n * 16
     for entry in _dict:
-        offsetDict[entry-offset] = _dict[entry]
-    
-    return offsetDict
-    
+        offset_dict[entry - offset] = _dict[entry]
+
+    return offset_dict
+
+
 def dict_merge(a, b):
-    '''Merges two dictionaries together'''
-    finalDict = a.copy()
-    finalDict.update(b)
-    return finalDict
-    
+    """Merges two dictionaries together"""
+    final_dict = a.copy()
+    final_dict.update(b)
+    return final_dict
+
+
 def decode_binary(_bin):
-    '''Handles decoding of the binary into text'''
+    """Handles decoding of the binary into text"""
     i = 0
     _str = ''
-    tempTBLDict = TBLDict.copy()
+    temp_tbl_dict = TBLDict.copy()
     try:
         while True:
-            if _bin[i] == 0xC8: # 0xC8 = <table>;
-                offsetTBLDict = offset_encoding(charTBLDict, _bin[i+1])
-                tempTBLDict = dict_merge(offsetTBLDict, codeTBLDict)
-        
-            #print(_str)
-            _str += tempTBLDict[_bin[i]]
+            if _bin[i] == 0xC8:  # 0xC8 = <table>;
+                offset_tbl_dict = offset_encoding(charTBLDict, _bin[i + 1])
+                temp_tbl_dict = dict_merge(offset_tbl_dict, codeTBLDict)
+
+            # print(_str)
+            _str += temp_tbl_dict[_bin[i]]
             i += 1
     except IndexError:
-        #print("Reached end of file")
-        return(_str)
+        # print("Reached end of file")
+        return _str
 
-# Load binary containing text script
-with open(binPath, 'rb') as f:
-    binScript = f.read()
 
-# Load character table file
-with open(charTBLPath, 'r') as f:
-    charTBLFile = f.read()
-charTBLDict = tbl2dict.convert(charTBLFile)
+if __name__ == "__main__":
+    # Load binary containing text script
+    with open(binPath, 'rb') as f:
+        binScript = f.read()
 
-# Load control code table file
-with open(codeTBLPath, 'r') as f:
-    codeTBLFile = f.read()
-codeTBLDict = tbl2dict.convert(codeTBLFile)
+    # Load character table file
+    with open(charTBLPath, 'r', encoding='UTF-8') as f:
+        charTBLFile = f.read()
+    charTBLDict = tbl2dict.convert(charTBLFile)
 
-TBLDict = dict_merge(charTBLDict, codeTBLDict)
+    # Load control code table file
+    with open(codeTBLPath, 'r') as f:
+        codeTBLFile = f.read()
+    codeTBLDict = tbl2dict.convert(codeTBLFile)
 
-# Profit
+    TBLDict = dict_merge(charTBLDict, codeTBLDict)
 
-#print(charmap_decode(binScript, 'strict', TBLDict))
-#offset_encoding(TBLDict, 1) 
-
-with open("[butt].txt", "w") as f:
-    f.write(decode_binary(binScript))
+    # Profit
+    with open("[butt].txt", "w", encoding='UTF-8') as f:
+        f.write(decode_binary(binScript))
